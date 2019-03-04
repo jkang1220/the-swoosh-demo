2e1b96a011c9525a0f21ab28a9911c635c9e00d5
Jimmy Kang <jimmykang@Jimmys-MacBook-Pro-2.local>
2019-03-03 22:49:01 -0500

    Fixed some resolvers and updated ReadME

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'jike',
-  },
+	client: 'mysql',
+	connection: {
+		host: '127.0.0.1',
+		user: 'root',
+		password: '',
+		database: 'jike',
+	},
 });
 
 // The GraphQL schema
@@ -40,7 +40,7 @@ const typeDefs = gql`
 		order_date: String
 		user_id: Int!
 		user: User!
-		products: [Product]
+		products: [Product]!
 		paid: Int
 		payment_date: String
 		order_tracking_number: String
@@ -48,6 +48,7 @@ const typeDefs = gql`
 
 	type Cart {
 		id: Int!
+		user: User!
 		products: [Product]!
 	}
 
@@ -89,7 +90,7 @@ const typeDefs = gql`
 		getAllUsers: [User]!
 		getUserByUserId(id: Int!): User
 		getAllOrders: [Order]!
-		getOrdersByUserId(id: Int!): Order
+		getOrdersByUserId(id: Int!): [Order]
 		getAllProducts: [Product]!
 		getProductById(id: Int!): Product
 		getAllCarts: [Cart]
@@ -107,87 +108,101 @@ const typeDefs = gql`
 
 // A map of functions which return data for the schema.
 const resolvers = {
-  User: {
-    cart: async parent => await knex.select().from('carts').where({ user_id: parent.id }).first(),
-    orders: async parent => await knex.select().from('orders').where({ user_id: parent.id }),
-  },
-  Order: {
-    user: async parent => await knex.select().from('users').where({ id: parent.user_id }).first(),
-    products: async parent => await knex.select('*').from('products').join('order_product', 'products.id', 'order_product.product_id').where('order_product.order_id', '=', parent.id),
-  },
-  Cart: {
-    products: async parent => (await knex.raw(`select products.* from (SELECT carts.user_id, cart_product.product_id FROM carts INNER JOIN cart_product ON carts.id = cart_product.cart_id) as wow INNER JOIN products ON wow.product_id = products.id where user_id = ${parent.user_id}`))[0],
-  },
-  Query: {
-    getAllUsers: async parent => await knex.select().from('users'),
-    getUserByUserId: async (parent, args) => await knex.select().from('users').where(args).first(),
-    getAllOrders: async () => await knex.select().from('orders'),
-    getOrdersByUserId: async (parent, args) => await knex.select().from('orders').where(args).first(),
-    getAllProducts: async () => await knex.select().from('products'),
-    getProductById: async (parent, args) => await knex('products').where(args).first(),
-    getAllCarts: async (parent, args) => await knex('cart').where(args).first(),
-    getCartByUserId: async (parent, args) => await knex('cart').where(args).first(),
-  },
-  Mutation: {
-    createOrder: async (parent, args) => {
-      await knex('orders')
-        .insert({
-          total: args.input.total,
-          order_date: args.input.order_date,
-          user_id: args.input.user_id,
-          paid: args.input.paid,
-          payment_date: args.input.payment_date,
-          order_tracking_number: args.input.order_tracking_number,
-        });
-      return await knex.select().from('orders').where({ id: createOrder[0] }).first();
-    },
-    AddProductsToOrder: async (parent, args) => {
-      const productIds = args.input.map(row => row.product_id);
+	User: {
+		cart: async parent => await knex.select().from('carts').where({ user_id: parent.id }).first(),
+		orders: async parent => await knex.select().from('orders').where({ user_id: parent.id }),
+	},
+	Order: {
+		user: async parent => await knex.select().from('users').where({ id: parent.user_id }).first(),
+		products: async (parent, args, context, info) => await knex.select('*').from('products').join('order_product', 'products.id', 'order_product.product_id').where('order_product.order_id', '=', parent.id),
+	},
+	Cart: {
+		user: async (parent, args) => { console.log('parent', parent); console.log('args', args); return await knex.select().from('users').where({ id: parent.user_id }).first() },
+		products: async (parent, args) => {
+			console.log('parent', parent);
+			console.log('args', args)
+			console.log((await knex.raw(
+				`select products.* from (SELECT carts.user_id, cart_product.product_id
+				FROM carts INNER JOIN cart_product ON carts.id = cart_product.cart_id WHERE carts.id = ${parent.id}) as wow
+				INNER JOIN products ON wow.product_id = products.id`))[0]);
+			return (await knex.raw(
+				`select products.* from (SELECT carts.user_id, cart_product.product_id
+				FROM carts INNER JOIN cart_product ON carts.id = cart_product.cart_id WHERE carts.id = ${parent.id}) as wow
+				INNER JOIN products ON wow.product_id = products.id`))[0];
+		},
+	},
+	Query: {
+		getAllUsers: async parent => await knex.select().from('users'),
+		getUserByUserId: async (parent, args) => await knex.select().from('users').where(args).first(),
+		getAllOrders: async () => await knex.select().from('orders'),
+		getOrdersByUserId: async (parent, args) => {
+			return await knex.select().from('orders').where('orders.user_id', '=', args.id)
+		},
+		getAllProducts: async () => await knex.select().from('products'),
+		getProductById: async (parent, args) => await knex('products').where(args).first(),
+		getAllCarts: async (parent, args) => await knex('carts'),
+		getCartByUserId: async (parent, args) => await knex('carts').where(args).first(),
+	},
+	Mutation: {
+		createOrder: async (parent, args) => {
+			await knex('orders')
+				.insert({
+					total: args.input.total,
+					order_date: args.input.order_date,
+					user_id: args.input.user_id,
+					paid: args.input.paid,
+					payment_date: args.input.payment_date,
+					order_tracking_number: args.input.order_tracking_number,
+				});
+			return await knex.select().from('orders').where({ id: createOrder[0] }).first();
+		},
+		AddProductsToOrder: async (parent, args) => {
+			const productIds = args.input.map(row => row.product_id);
 
-      args.input.forEach(async (productToOrderRelationship) => {
-        await knex('order_product').insert(
-          productToOrderRelationship,
-        );
-      });
+			args.input.forEach(async (productToOrderRelationship) => {
+				await knex('order_product').insert(
+					productToOrderRelationship,
+				);
+			});
 
-      return await knex('products').whereIn('products.id', [...productIds]);
-    },
-    AddItemToCart: async (parent, args) => {
-      await knex('cart_product')
-        .insert({
-          cart_id: args.input.cart_id,
-          product_id: args.input.product_id,
-        });
-      return await knex.select().from('products').where({ id: args.input.product_id }).first();
-    },
-    RemoveItemFromCart: async (parent, args) => {
-      const removedItem = await knex('cart_product')
-        .where({
-          cart_id: args.input.cart_id,
-          product_id: args.input.product_id,
-        }).del();
-      return removedItem;
-    },
-    AddProductToCatalog: async (parent, args) => {
-      const addedProductId = (await knex('products')
-        .insert({
-          name: args.input.name,
-          description: args.input.description,
-          retail_price: args.input.retail_price,
-          product_sku: args.input.product_sku,
-          img: args.input.img,
-          stock: args.input.stock,
-        }))[0];
-      return await knex('products').where({ id: addedProductId }).first();
-    },
-  },
+			return await knex('products').whereIn('products.id', [...productIds]);
+		},
+		AddItemToCart: async (parent, args) => {
+			await knex('cart_product')
+				.insert({
+					cart_id: args.input.cart_id,
+					product_id: args.input.product_id,
+				});
+			return await knex.select().from('products').where({ id: args.input.product_id }).first();
+		},
+		RemoveItemFromCart: async (parent, args) => {
+			const removedItem = await knex('cart_product')
+				.where({
+					cart_id: args.input.cart_id,
+					product_id: args.input.product_id,
+				}).del();
+			return removedItem;
+		},
+		AddProductToCatalog: async (parent, args) => {
+			const addedProductId = (await knex('products')
+				.insert({
+					name: args.input.name,
+					description: args.input.description,
+					retail_price: args.input.retail_price,
+					product_sku: args.input.product_sku,
+					img: args.input.img,
+					stock: args.input.stock,
+				}))[0];
+			return await knex('products').where({ id: addedProductId }).first();
+		},
+	},
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+	typeDefs,
+	resolvers,
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀 Apollo Server ready at ${url}`);
+	console.log(`🚀 Apollo Server ready at ${url}`);
 });
